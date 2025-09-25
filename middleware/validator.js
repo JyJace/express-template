48878e06f8d8a2d3004130f6445cbebc26aeb484
pj lin <>
2025-09-25 09:51:38 +0800

    修复校验器被覆盖的问题

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,4 +1,3 @@
-// const authValidator = require("../validators/authValidator")
 const { validationResult } = require('express-validator');
 
 /**
@@ -6,11 +5,10 @@ const { validationResult } = require('express-validator');
  * @param name 校验器的名 存放在validators文件夹下
  * @return {(function(*, *, *): Promise<void>)|*}
  */
-let validatorModule = ''
 module.exports = (name) => {
 
     if (!name) throw new Error(`validator中间件 需要传入一个校验器名`);
-    validatorModule =  require(`../validators/${name}`);
+    const validatorModule =  require(`../validators/${name}`);
 
     return async (req, res, next) => {
 
@@ -18,16 +16,11 @@ module.exports = (name) => {
             throw new Error('校验规则必须是一个数组');
         }
 
-
         for (const validation of validatorModule.validationRules) {
 
             await new Promise((resolve,reject) => {
                 validation(req, res, (err) => {
-                    if (err) {
-                        reject(err);
-                    }else{
-                        resolve();
-                    }
+                    resolve(err);
                 });
             })
 
